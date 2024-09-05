36207fb3307a600110eb5bb4840bfc20cd03c5b3
Wyldson Marllon <wyldsonmarllon39@gmail.com>
2024-09-04 21:59:57 -0300

    Criando os arquivos em JS

---
+++ dados.js
@@ -0,0 +1,18 @@
+let atletas = [
+    {
+        nome: "Rebeca Andrade",
+        descricao: "Rebeca Rodrigues de Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com seu talento, determinação e carisma, Rebeca já fez história na ginástica e continua a inspirar novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        nome: "Beatriz Souza" ,
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, na categoria acima de 78kg. Sua técnica e força fizeram dela uma das grandes estrelas do judô mundial.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        nome: "Ana Patrícia e Duda",
+        descricao: "A dupla de vôlei de praia formada por Ana Patrícia e Duda conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, mostrando um jogo impecável e muita sincronia. Elas se tornaram as grandes campeãs da modalidade.",
+        link: "https://pt.wikipedia.org/wiki/Ana_Patrícia_Rego",
+        link2: "https://pt.wikipedia.org/wiki/Duda_Lisboa"
+    }
+]
